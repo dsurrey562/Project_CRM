321418037defc652687f1a2b02acf035042072ef
David Surrey <dsurrey562@gmail.com>
2021-09-21 21:52:30 -0700

    pushing updates to git

--- ProjCRM/account01/views.py
+++ ProjCRM/account01/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+  return render(request, 'account01/dashboard.html')
+
+def products(request):
+  return render(request, 'account01/products.html')
+
+def customer(request):
+  return render(request, 'account01/customer.html')
